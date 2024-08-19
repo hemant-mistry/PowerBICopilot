ef1d1bda66a533f078b28614e436b756cecb7505
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-08-19 17:55:00 +0530

    Update Chatbot.jsx

--- Frontend/powerbi-copilot-ui/src/components/Chatbot.jsx
+++ Frontend/powerbi-copilot-ui/src/components/Chatbot.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import axios from "axios";
 import sendIcon from "../assets/sendicon.png";
+import promptGuide from "../assets/prompt-guide.png";
 import ReactMarkdown from "react-markdown";
 import BarChart from "./Charts/BarChart";
 import AreaChart from "./Charts/AreaChart";
@@ -16,6 +17,7 @@ function Chatbot() {
   const [loading, setLoading] = useState(false);
   const [chartData, setChartData] = useState(null);
   const [tableData, setTableData] = useState(null);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
 
   useEffect(() => {
     if (messagesEndRef.current) {
@@ -140,9 +142,7 @@ function Chatbot() {
                   ) : (
                     <tr>
                       <td
-                        colSpan={
-                          Object.keys(rows[0]).length || 1
-                        }
+                        colSpan={Object.keys(rows[0]).length || 1}
                         className="px-6 py-4 text-center"
                       >
                         No Data
@@ -160,7 +160,6 @@ function Chatbot() {
           ...prevMessages.slice(0, -1),
           botMessage,
         ]);
-        
       } else {
         throw new Error("Unexpected response structure");
       }
@@ -168,7 +167,8 @@ function Chatbot() {
       console.error("Error sending message:", error.message);
       const errorMessage = {
         role: "bot",
-        content: "There was an error processing your request. Please try again.",
+        content:
+          "There was an error processing your request. Please try again.",
       };
 
       // Replace the "Processing..." message with the error message
@@ -180,6 +180,55 @@ function Chatbot() {
       setLoading(false);
     }
   };
+
+  const handlePromptClick = (prompt) => {
+    setMessage(prompt);
+    handleSendMessage();
+    setIsMenuVisible(false);
+  };
+
+  const PromptGuideMenu = ({ handlePromptClick }) => (
+    <div className="absolute bottom-full left-0 mb-2 w-full border bg-black max-w-sm shadow-lg rounded-lg p-4">
+      <h3 className="text-lg font-bold mb-2">Prompt Guide</h3>
+      <ul className=" list-inside">
+        <li
+          onClick={() =>
+            handlePromptClick("Show me the sum of profit by category")
+          }
+          className="cursor-pointer hover:text-blue-600"
+        >
+          Show me the sum of profit by category
+        </li>
+        
+        <li
+          onClick={() =>
+            handlePromptClick(
+              "Show me the top 5 products with highest sales"
+            )
+          }
+          className="cursor-pointer hover:text-blue-600"
+        >
+         Show me the top 5 products with highest sales
+        </li>
+        <li
+          onClick={() =>
+            handlePromptClick("Can you show me sales by segment in a pie chart?")
+          }
+          className="cursor-pointer hover:text-blue-600"
+        >
+          Can you show me sales by segment in a pie chart?
+        </li>
+        <li
+          onClick={() =>
+            handlePromptClick("Show me the top 3 countries with the highest sales")
+          }
+          className="cursor-pointer hover:text-blue-600"
+        >
+          Show me the top 3 countries with the highest sales
+        </li>
+      </ul>
+    </div>
+  );
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
@@ -202,7 +251,8 @@ function Chatbot() {
           <div className="flex flex-col text-center mx-auto w-full max-w-2xl">
             <h1 className="lg:text-5xl sm:text-5xl font-bold">Welcome back!</h1>
             <p className="py-6 lg:text-lg sm:text-lg">
-              Get to know how you spent on your trip with the help of Finance AI.
+              This is a POC Power BI Custom Copilot trained on the semantic
+              model.
               <br />
               Select one of the prompts mentioned below to get started.
             </p>
@@ -242,42 +292,34 @@ function Chatbot() {
               onChange={(e) => setMessage(e.target.value)}
               onKeyDown={handleKeyPress}
               placeholder="Type your message..."
-              className="w-full rounded-lg p-2 resize-none pr-12"
+              className="w-full rounded-lg p-2 resize-none pr-[70px]"
             />
             <div className="flex items-center absolute right-2 top-0 h-full">
               <button
-                className="flex items-center justify-center"
+                className="btn"
+                onClick={() => setIsMenuVisible((prev) => !prev)}
+              >
+                 <img src={promptGuide} alt="prompt-guide" className="w-6 h-6" />
+              </button>
+              <button
+                className="flex items-center justify-center ml-5"
                 onClick={handleSendMessage}
               >
                 <img src={sendIcon} alt="Send" className="w-6 h-6" />
               </button>
             </div>
           </div>
+          {isMenuVisible && (
+            <PromptGuideMenu handlePromptClick={handlePromptClick} />
+          )}
         </div>
+
         <div className="prompt-caution text-center text-xs mt-2">
-          Gemini may display inaccurate info, including about people, so
-          double-check its responses.{" "}
-          <a
-            className="link link-primary"
-            href="https://support.google.com/gemini?p=privacy_notice"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Privacy Notice
-          </a>{" "}
-          |{" "}
-          <a
-            className="link link-primary"
-            href="https://support.google.com/gemini?p=faqs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            FAQs
-          </a>
+          Responses may display inaccurate info, including about people, so
+          double-check its responses.
         </div>
       </div>
     </>
   );
 }
-
 export default Chatbot;
