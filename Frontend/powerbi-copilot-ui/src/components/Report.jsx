30869716b93f50d23c0ac0b6e80d3fb7426ce09d
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-09-17 15:44:22 +0530

    Update Report.jsx

--- Frontend/powerbi-copilot-ui/src/components/Report.jsx
+++ Frontend/powerbi-copilot-ui/src/components/Report.jsx
@@ -4,7 +4,7 @@ function Report(){
             <div className="bg-gray-100 p-5 rounded-[10px]">
                 <div className="text-lg text-black mb-3 text-center font-bold">Power BI Report</div>
                 <iframe 
-                    title="Sales Report CopilotPOC" 
+                    title="Sales Report" 
                     width="950" 
                     height="541.25" 
                     src="https://app.powerbi.com/reportEmbed?reportId=64c9fd97-1325-450c-992f-211a72059955&autoAuth=true&ctid=e4d98dd2-9199-42e5-ba8b-da3e763ede2e&filterPaneEnabled=false&navContentPaneEnabled=false" 
